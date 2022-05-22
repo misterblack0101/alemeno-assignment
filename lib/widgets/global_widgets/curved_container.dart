2a3ecb23ec6019a5b49b154ce4f6f82bdb41ef09
Harsh M <>
2022-05-22 09:22:53 +0530

    created curved container widget

---
+++ lib/widgets/global_widgets/curved_container.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CurvedContainer extends StatelessWidget {
+  const CurvedContainer({
+    Key? key,
+    required this.displayText,
+    required this.iconButton,
+    required this.mainWidget,
+  }) : super(key: key);
+  final String displayText;
+  final Widget iconButton;
+
+  // This is the widget that takes the most space.
+  // This is either the capture widget, or the image widget.
+  final Widget mainWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFFF4F4F4),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(40.0),
+          topLeft: Radius.circular(40.0),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          mainWidget,
+          Text(
+            displayText,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              color: Color(0xFF3C3C3C),
+              fontSize: 24,
+            ),
+          ),
+          iconButton,
+        ],
+      ),
+    );
+  }
+}
