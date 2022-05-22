1dd596e828328e9eadfe7a26739274bccebdcd3d
Harsh M <>
2022-05-22 09:45:52 +0530

    Created layout for click picture screen.

--- lib/screens/click_picture.dart
+++ lib/screens/click_picture.dart
@@ -1,3 +1,4 @@
+import 'package:alemeno_assignment/global_constants.dart';
 import 'package:alemeno_assignment/widgets/global_widgets/base_layout.dart';
 import 'package:alemeno_assignment/widgets/global_widgets/curved_container.dart';
 import 'package:flutter/material.dart';
@@ -7,14 +8,43 @@ class ClickPictureScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return BaseLayout(
       curvedContainer: CurvedContainer(
         displayText: "Click your meal",
-        iconButton: IconButton(
-          icon: const Icon(Icons.camera),
+        iconButton: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            padding: const EdgeInsets.all(10),
+            primary: CustomColors.primaryColor,
+          ),
+          child: const Icon(
+            Icons.camera_alt,
+            size: 30,
+          ),
           onPressed: () {},
         ),
-        mainWidget: Container(),
+        mainWidget: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset("assets/images/fork.png"),
+            Container(
+              width: size.width * 0.5,
+              height: size.width * 0.5,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: CustomColors.primaryColor,
+                  width: 2,
+                ),
+              ),
+              child: CircleAvatar(
+                radius: size.width * 0.2,
+              ),
+            ),
+            Image.asset("assets/images/spoon.png"),
+          ],
+        ),
       ),
     );
   }
